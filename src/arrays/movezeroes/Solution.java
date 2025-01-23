8550881d1381bddb1f911f038951b23e76c8b3da
Nazakat Nazir Sofi <sofinazakat786@gmail.com>
2025-01-23 15:54:43 +0530

    test

---
+++ src/arrays/movezeroes/Solution.java
@@ -0,0 +1,34 @@
+package arrays.movezeroes;
+
+class Solution {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = {0, 1, 0, 3, 12};
+
+        solution.moveZeroes(nums);
+
+        System.out.print("Array after moving zeroes:");
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
+    }
+
+    public void moveZeroes(int[] nums) {
+        int nonZeroIndex = 0; // Pointer for the next non-zero element
+
+        // Move all non-zero elements to the front
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[nonZeroIndex] = nums[i]; // Place the non-zero element at the "nonZeroIndex"
+                nonZeroIndex++;
+            }
+        }
+
+        // Fill the remaining positions with 0
+        for (int i = nonZeroIndex; i < nums.length; i++) {
+            nums[i] = 0;
+        }
+    }
+
+}
