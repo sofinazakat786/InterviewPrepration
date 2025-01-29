cd3ebd9c8948f95f55672300fcc82ba18063d233
Nazakat Nazir Sofi <sofinazakat786@gmail.com>
2025-01-29 16:45:32 +0530

    practice array

---
+++ src/arrays/arraybasics/SumArray.java
@@ -0,0 +1,20 @@
+package arrays.arraybasics;
+import java.util.Arrays;
+
+public class SumArray {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5};
+        System.out.println("Array elements: "+ Arrays.toString(arr));
+        int sum = sumArray(arr);
+        System.out.println("Sum of array elements: "+sum);
+
+    }
+    public static int sumArray(int[] arr){
+        int sum = 0;
+        for(int i= 0 ;i < arr.length; i++){
+            sum+= arr[i];
+        }
+        return sum;
+    }
+
+}
