b790c04b06c07557feaaadcb840f2614937b21a4
Nazakat Nazir Sofi <sofinazakat786@gmail.com>
2025-02-18 17:18:56 +0530

    implemented stack using array and linkedlist

---
+++ src/stack/stackImplemention/usingLinkedList/Node.java
@@ -0,0 +1,11 @@
+package stack.stackImplemention.usingLinkedList;
+
+public class Node {
+    int data;
+    Node next;
+
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
